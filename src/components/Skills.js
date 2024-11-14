2986bae39afbeee08fcf70092bc385daa2133bf3
raghavnimbalkar1 <raghav.nimbalkar@gmail.com>
2024-11-14 16:01:21 +0530

    updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,24 +1,22 @@
 import React from "react";
 
-const skills = [
-  "NodeJs",
-  "ReactJs",
-  "Python",
-  "MERN Stack",
-  "MongoDB",
-  "C",
-  "HTML, CSS",
-];
+const skills = {
+  Languages: ["Python" , "JavaScript" , "C"],
+  "Web Technologies": ["NumPy" , "Pandas" , "ReactJS" , "Node.js" , "ExpressJs" , "Bootstrap" , "HTML" , "CSS"],
+  Database: ["MongoDB"],
+  Tools: ["Git" , "GitHub" , "Postman" , "Visual Studio" , "Photoshop" , "Godot Engine"],
+};
 
 function Skills() {
   return (
     <section className="skills">
       <h2>Skills</h2>
-      <div className="skill-tags">
-        {skills.map((skill, index) => (
-          <span className="skill-tag" key={index}>
-            {skill}
-          </span>
+      <div className="skills-content">
+        {Object.entries(skills).map(([category, items], index) => (
+          <div className="skill-category" key={index}>
+            <span className="skill-title">{category}:</span>
+            <span className="skill-items">{items.join(", ")}</span>
+          </div>
         ))}
       </div>
     </section>
